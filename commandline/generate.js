c999c687fb46db84ee836364265986310fd7077f
William Pettersson <william@ewpettersson.se>
2020-08-11 10:09:22 +0100

    Updates to command line script
    - get sizes from config
    - support tuning (blood groups and donor counts)
    - Support saving to files based on config

--- commandline/generate.js
+++ commandline/generate.js
@@ -4,41 +4,60 @@ const fs = require('fs');
 
 var args = process.argv.slice(2);
 
-var numPatients = +args[0];
-var numInstances = +args[1];
-var tune = args[2] || false;
+var configFile = args[0] || 'saidman-config.json';
 
-let cfg = JSON.parse(fs.readFileSync('saidman-config.json'));
+var config = JSON.parse(fs.readFileSync(configFile));
 
-var saidmanCfg = cfg;
-saidmanCfg.donorBtDistribution = new BloodTypeDistribution(
-    cfg.donorBtDistribution.probO,
-    cfg.donorBtDistribution.probA,
-    cfg.donorBtDistribution.probB,
-    cfg.donorBtDistribution.probAB);
-saidmanCfg.patientBtDistribution = new BloodTypeDistribution(
-    cfg.patientBtDistribution.probO,
-    cfg.patientBtDistribution.probA,
-    cfg.patientBtDistribution.probB,
-    cfg.patientBtDistribution.probAB);
+config.donorBtDistribution = new BloodTypeDistribution(
+    config.donorBtDistribution.probO,
+    config.donorBtDistribution.probA,
+    config.donorBtDistribution.probB,
+    config.donorBtDistribution.probAB);
+config.patientBtDistribution = new BloodTypeDistribution(
+    config.patientBtDistribution.probO,
+    config.patientBtDistribution.probA,
+    config.patientBtDistribution.probB,
+    config.patientBtDistribution.probAB);
 
-if (tune) {
-  var tuneIter = 50;
-  var tuneError = 0.005;
-  saidmanCfg = TuneConfig(saidmanCfg, tuneIter, tuneError);
+if (config.tuneBts) {
+  var tuneIter = config.tuneBts.iters || 50;
+  var tuneError = config.tuneBts.error || 0.005;
+  var tuneSize = config.tuneBts.size || config.patientsPerInstance;
+  config = TuneConfig(config, tuneIter, tuneError, tuneSize);
 }
 
-var gen = new KidneyGenerator(saidmanCfg);
+if (config.tuneDonors) {
+  var tuneIter = config.tuneDonors.iters || 50;
+  var tuneError = config.tuneDonors.error || 0.005;
+  var tuneSize = config.tuneDonors.size || config.patientsPerInstance;
+  config = TuneDonorConfig(config, tuneIter, tuneError, tuneSize);
+}
 
-for (var i=0; i<numInstances; i++) {
-  var generatedDataset = gen.generateDataset(numPatients, 0);
+var gen = new KidneyGenerator(config);
+
+for (var i=0; i < config.numberOfInstances; i++) {
+  var generatedDataset = gen.generateDataset(config.patientsPerInstance, config.proportionAltruistic);
   var d = generatedDataset.data;
-  for (var j=0; j<d.length; j++) {
-    var val = d[j];
-    if ("matches" in val) {
-      console.log(val.matches.length);
+  if (config.testing) {
+    for (var j=0; j<d.length; j++) {
+      var val = d[j];
+      if ("matches" in val) {
+        console.log(val.matches.length);
+      } else {
+        console.log(0);
+      }
+    }
+  }
+  if (! config.testing) {
+    var filename = config.outputName || "Generated";
+    filename += "_" + config.patientsPerInstance;
+    var format = config.outputFormat || "json";
+    filename += "_" + i + "." + format;
+    var nullfn = function() {};
+    if (format == "json") {
+      fs.writeFile(filename, generatedDataset.toJsonString(), nullfn);
     } else {
-      console.log(0);
+      fs.writeFile(filename, generatedDataset.toXmlString(), nullfn);
     }
   }
 }
