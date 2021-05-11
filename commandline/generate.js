0c2341e37e771e21b770f7ae25461eca36d783db
William Pettersson <william@ewpettersson.se>
2021-05-11 10:22:29 +0100

    Update generate script
    Allows new features to be used

--- commandline/generate.js
+++ commandline/generate.js
@@ -8,37 +8,59 @@ var configFile = args[0] || 'saidman-config.json';
 
 var config = JSON.parse(fs.readFileSync(configFile));
 
-config.donorBtDistribution = new BloodTypeDistribution(
-    config.donorBtDistribution.probO,
-    config.donorBtDistribution.probA,
-    config.donorBtDistribution.probB,
-    config.donorBtDistribution.probAB);
 config.patientBtDistribution = new BloodTypeDistribution(
     config.patientBtDistribution.probO,
     config.patientBtDistribution.probA,
     config.patientBtDistribution.probB,
     config.patientBtDistribution.probAB);
 
-if (config.tuneBts) {
-  var tuneIter = config.tuneBts.iters || 50;
-  var tuneError = config.tuneBts.error || 0.005;
-  var tuneSize = config.tuneBts.size || config.patientsPerInstance;
-  config = TuneConfig(config, tuneIter, tuneError, tuneSize);
+if (config.donorBtDistributionByPatientO) {
+  config.donorBtDistributionByPatientO = new BloodTypeDistribution(
+    config.donorBtDistributionByPatientO.probO,
+    config.donorBtDistributionByPatientO.probA,
+    config.donorBtDistributionByPatientO.probB,
+    config.donorBtDistributionByPatientO.probAB)
+  config.donorBtDistributionByPatientA = new BloodTypeDistribution(
+    config.donorBtDistributionByPatientA.probO,
+    config.donorBtDistributionByPatientA.probA,
+    config.donorBtDistributionByPatientA.probB,
+    config.donorBtDistributionByPatientA.probAB)
+  config.donorBtDistributionByPatientB = new BloodTypeDistribution(
+    config.donorBtDistributionByPatientB.probO,
+    config.donorBtDistributionByPatientB.probA,
+    config.donorBtDistributionByPatientB.probB,
+    config.donorBtDistributionByPatientB.probAB)
+  config.donorBtDistributionByPatientAB = new BloodTypeDistribution(
+    config.donorBtDistributionByPatientAB.probO,
+    config.donorBtDistributionByPatientAB.probA,
+    config.donorBtDistributionByPatientAB.probB,
+    config.donorBtDistributionByPatientAB.probAB)
+  config.donorBtDistributionByPatientNDD = new BloodTypeDistribution(
+    config.donorBtDistributionByPatientNDD.probO,
+    config.donorBtDistributionByPatientNDD.probA,
+    config.donorBtDistributionByPatientNDD.probB,
+    config.donorBtDistributionByPatientNDD.probAB)
+} else {
+  config.donorBtDistribution = new BloodTypeDistribution(
+    config.donorBtDistribution.probO,
+    config.donorBtDistribution.probA,
+    config.donorBtDistribution.probB,
+    config.donorBtDistribution.probAB);
 }
 
-if (config.tuneDonors) {
-  var tuneIter = config.tuneDonors.iters || 50;
-  var tuneError = config.tuneDonors.error || 0.005;
-  var tuneSize = config.tuneDonors.size || config.patientsPerInstance;
-  config = TuneDonorConfig(config, tuneIter, tuneError, tuneSize);
+if (config.tune) {
+  var tuneIter = config.tune.iters || 50;
+  var tuneError = config.tune.error || 0.005;
+  var tuneSize = config.tune.size || config.patientsPerInstance;
+  config = TuneConfig(config, tuneIter, tuneError, tuneSize);
 }
 
 var gen = new KidneyGenerator(config);
 
 for (var i=0; i < config.numberOfInstances; i++) {
   var generatedDataset = gen.generateDataset(config.patientsPerInstance, config.proportionAltruistic);
-  var d = generatedDataset.data;
   if (config.testing) {
+    var d = generatedDataset.data;
     for (var j=0; j<d.length; j++) {
       var val = d[j];
       if ("matches" in val) {
@@ -54,10 +76,11 @@ for (var i=0; i < config.numberOfInstances; i++) {
     var format = config.outputFormat || "json";
     filename += "_" + i + "." + format;
     var nullfn = function() {};
+    var fullDetails = config.fullDetails == "true";
     if (format == "json") {
-      fs.writeFile(filename, generatedDataset.toJsonString(), nullfn);
+      fs.writeFile(filename, generatedDataset.toJsonString(fullDetails), nullfn);
     } else {
-      fs.writeFile(filename, generatedDataset.toXmlString(), nullfn);
+      fs.writeFile(filename, generatedDataset.toXmlString(fullDetails), nullfn);
     }
   }
 }
